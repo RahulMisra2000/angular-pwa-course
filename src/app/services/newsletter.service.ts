81fe1b73c64581bb73f6602fde1351a61a3b4214
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-28 21:47:01 -0400

    Comments

--- src/app/services/newsletter.service.ts
+++ src/app/services/newsletter.service.ts
@@ -8,10 +8,11 @@ import {HttpClient} from "@angular/common/http";
 @Injectable()
 export class NewsletterService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient) { }
 
-    }
-
+    // *** Just sending the subscription object to a Web API Backend so that it can be persisted for later use.
+    //     Later another Web API call can be made that will send message with the subscription object to a Push Notification Service
+    //     like Google Firebase Cloud Messaging (for Chrome)
     addPushSubscriber(sub:any) {
         return this.http.post('/api/notifications', sub);
     }
