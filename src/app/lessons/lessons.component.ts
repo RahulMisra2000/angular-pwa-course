94dfd6ea9fd730c59d91680ccca5995cdefdfa52
RahulMisra2000 <RahulMisra2000@users.noreply.github.com>
2018-06-28 21:43:24 -0400

    Comments

--- src/app/lessons/lessons.component.ts
+++ src/app/lessons/lessons.component.ts
@@ -37,21 +37,21 @@ export class LessonsComponent implements OnInit {
 
     subscribeToNotifications() {
 
+        // This will popup a window asking the User if they give permission for subscription **********************
         this.swPush.requestSubscription({
-            serverPublicKey: this.VAPID_PUBLIC_KEY
+                      // The Web API backend has a VAPID Private Key and this is the corresponding public key
+                      serverPublicKey: this.VAPID_PUBLIC_KEY
         })
         .then(sub => {
-
             this.sub = sub;
-
-
             console.log("Notification Subscription: ", sub);
 
+            // Sending the subscription object to a Web API backend that will persist it for later
+            // sending notification to this user/subscriber
             this.newsletterService.addPushSubscriber(sub).subscribe(
                 () => console.log('Sent push subscription object to server.'),
                 err =>  console.log('Could not send subscription object to server, reason: ', err)
             );
-
         })
         .catch(err => console.error("Could not subscribe to notifications", err));
 
@@ -59,10 +59,7 @@ export class LessonsComponent implements OnInit {
 
 
     sendNewsletter() {
-
-
         console.log("Sending Newsletter to all Subscribers ...");
-
         this.newsletterService.send().subscribe();
     }
 
